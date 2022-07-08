f5b6925c433b0b9c6384262f9c4cbc8d503bdf30
mmdohmen <mmdohmen@hotmail.com>
2022-07-08 12:14:16 -0300

    1er commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from "./task"
+
+// creo un array con las tareas p/ emular una bbdd
+export const TASKS :Task[] = [
+    {
+        id: 1,
+        text: 'terminar video Angular My Task List',
+        day: '08-julio',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'hacer Ejercicio Obligatorio 5 Node JS',
+        day: '10-julio',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'empezar sitio E-commerce',
+        day: '11-julio',
+        reminder: true
+    },
+
+]
---
+++ src/app/task.ts
@@ -0,0 +1,7 @@
+// creo una INTERFAZ para definir los datos de cada tarea
+export interface Task {
+    id : number,
+    text : string,
+    day : string,
+    reminder : boolean
+}
